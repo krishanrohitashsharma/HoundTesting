374575b6f3ee2238c1a97bbae1a7799bff88cd04
Krishan kumar <krishankumar@krishans-macbook-pro.local>
2019-04-19 19:20:08 +0530

    func created

--- HoundTesting/ViewController.swift
+++ HoundTesting/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    var a:String=""
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func show()
+    {
+        
+    }
 
 }
 
